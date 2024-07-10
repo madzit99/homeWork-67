4df0f08ee29b12aff700ceee368797518b6ba909
Aliev Madjit <madzitaliev51@gmail.com>
2024-07-10 19:09:24 +0600

    Сделал проверку на правильный код от двери и стили

--- src/Door/Door.tsx
+++ src/Door/Door.tsx
@@ -1,20 +1,40 @@
 import { useDispatch, useSelector } from "react-redux";
-import { addValue } from "./doorSlice";
+import { addValue, checkCode, disableButtons, slice } from "./doorSlice";
 import { RootState } from "../app/store";
+import { useEffect } from "react";
 
 const Door = () => {
-  const numbersButtons = useSelector((state: RootState) => state.door.value);
+  const doorValue = useSelector((state: RootState) => state.door.value);
+  const disabled = useSelector((state: RootState) => state.door.disable);
+  const checked = useSelector((state: RootState) => state.door.checked);
+  const color = useSelector((state: RootState) => state.door.color)
+  const numbersButtons = useSelector((state: RootState) => {
+    if (state.door.value.length > 0 && state.door.text !== "") {
+      return state.door.text;
+    } else {
+      return state.door.value.replace(/./g, "*");
+    }
+  });
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (doorValue.length >= 4) {
+      dispatch(disableButtons(true));
+    } else {
+      dispatch(disableButtons(false));
+    }
+  }, [doorValue]);
 
   return (
     <div className="d-flex flex-column border w-50 mx-auto">
-      <div className="pt-3 pb-3 text-center">{numbersButtons}</div>
+      <div className={`pt-3 pb-3 text-center mb-2 ${color}`}>{numbersButtons}</div>
       <div className="row justify-content-center mb-3">
         <div className="col-3">
           <button
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("7"))}
+            disabled={disabled}
           >
             7
           </button>
@@ -24,6 +44,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("8"))}
+            disabled={disabled}
           >
             8
           </button>
@@ -33,6 +54,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("9"))}
+            disabled={disabled}
           >
             9
           </button>
@@ -44,6 +66,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("4"))}
+            disabled={disabled}
           >
             4
           </button>
@@ -53,6 +76,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("5"))}
+            disabled={disabled}
           >
             5
           </button>
@@ -62,6 +86,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("6"))}
+            disabled={disabled}
           >
             6
           </button>
@@ -73,6 +98,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("1"))}
+            disabled={disabled}
           >
             1
           </button>
@@ -82,6 +108,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("2"))}
+            disabled={disabled}
           >
             2
           </button>
@@ -91,6 +118,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("3"))}
+            disabled={disabled}
           >
             3
           </button>
@@ -101,7 +129,8 @@ const Door = () => {
           <button
             type="button"
             className="btn btn-secondary btn-lg btn-block"
-            onClick={() => dispatch(addValue(""))}
+            onClick={() => dispatch(slice())}
+            disabled={checked}
           >
             &lt;
           </button>
@@ -111,6 +140,7 @@ const Door = () => {
             type="button"
             className="btn btn-secondary btn-lg btn-block"
             onClick={() => dispatch(addValue("0"))}
+            disabled={disabled}
           >
             0
           </button>
@@ -119,7 +149,8 @@ const Door = () => {
           <button
             type="button"
             className="btn btn-secondary btn-lg btn-block"
-            onClick={() => dispatch(addValue(""))}
+            onClick={() => dispatch(checkCode())}
+            disabled={checked}
           >
             E
           </button>
--- src/Door/doorSlice.ts
+++ src/Door/doorSlice.ts
@@ -4,12 +4,18 @@ interface doorState {
   value: string;
   text: string;
   code: number;
+  disable: boolean;
+  checked: boolean;
+  color: string;
 }
 
 const initialState: doorState = {
   value: "",
   text: "",
   code: 1337,
+  disable: false,
+  checked: false,
+  color: ""
 };
 
 export const doorSlice = createSlice({
@@ -20,9 +26,28 @@ export const doorSlice = createSlice({
       state.value += action.payload;
       console.log(state.value);
     },
+    slice: (state) => {
+      if (state.value.length > 0) {
+        state.value = state.value.slice(0, -1);
+      }
+    },
+    disableButtons: (state, action: PayloadAction<boolean>) => {
+      state.disable = action.payload;
+    },
+    checkCode: (state) => {
+      const number = parseInt(state.value);
+      if (number === state.code) {
+        state.text = "Доступ разрешен!";
+        state.color = "bg-success";
+      } else {
+        state.text = "Доступ запрещен!";
+        state.color = "bg-danger";
+      }
+      state.checked = true;
+    },
   },
 });
 
 export const doorReducer = doorSlice.reducer;
 
-export const { addValue } = doorSlice.actions;
+export const { addValue, slice, disableButtons, checkCode } = doorSlice.actions;
